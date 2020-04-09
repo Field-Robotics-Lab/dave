95e391630578d93a5e3f65bfbef075326a894b15
Duane Davis <dtdavi1@nps.edu>
2020-04-09 15:48:10 -0700

    Example implementation of the UUV simulator DVL plugin.
    The teledyne_whn_standalone launch file instantiates a single
    standalone DVL model that utilizes the plugin in the uuv_dave
    Gazebo world.
    
    Relevant topics are dvl/dvl and dvl/dvl_twist.

---
+++ dvl_gazebo/src/simple_motion.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# Instantiates a ROS node that sets a fixed velocity to a standalone
+# Teledyne WHN DVL model in the gazebo scene.  The velocity is set
+# relative to the DVL base link at a rate of 50 hertz to effect a slow
+# right hand turn (0.25 radians per second) with a forward velocity of
+# 1 meter per second and a descent rate of 0.5 meters per scend
+#-----------------------------------------------------------------------
+
+import rospy
+import gazebo_msgs.msg as gm
+import time
+
+#SERVICE_NAME = 'gazebo/set_model_state'
+TOPIC_NAME = 'gazebo/set_model_state'
+MODEL_NAME = 'teledyne_whn'
+LINK_NAME = 'dvl_link'
+
+if __name__ == '__main__':
+    rospy.init_node('apply_velocity')
+    rate = rospy.Rate(50)
+    publisher = rospy.Publisher(TOPIC_NAME, gm.ModelState, queue_size=1)
+    command = gm.ModelState()
+    command.model_name = MODEL_NAME
+    command.reference_frame = LINK_NAME
+    command.twist.linear.y = 1.0
+    command.twist.linear.x = 0.5
+    command.twist.angular.x = 0.25
+    time.sleep(10)  # Give things time to start up
+    while not rospy.is_shutdown():
+        publisher.publish(command)
+        rate.sleep()
