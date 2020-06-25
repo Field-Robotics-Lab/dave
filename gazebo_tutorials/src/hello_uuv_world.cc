12c1ee4bacfc2db8f73a84356dc6e5e08383a7e4
Youssef <youssefkhaky@hotmail.com>
2020-06-25 16:35:32 -0400

    Add update function

--- gazebo_tutorials/src/hello_uuv_world.cc
+++ gazebo_tutorials/src/hello_uuv_world.cc
@@ -4,13 +4,28 @@ namespace gazebo
 {
   class WorldUuvPlugin : public WorldPlugin
   {
-    public: WorldUuvPlugin() : WorldPlugin()
+  private: physics::WorldPtr world;
+  private: physics::ModelPtr socket;
+  private: physics::ModelPtr plug;
+  private: gazebo::event::ConnectionPtr updateConnection;
+
+  public: WorldUuvPlugin() : WorldPlugin()
             {
               printf("Hello World!\n");
             }
 
-    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+  public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
             {
+              this->world = _world;
+              this->socket = this->world->GetModel("atlas");
+              this->plug = this->world->GetModel("atlas");
+              this->updateConnection = gazebo::event::Events::ConnectWorldUpdateBegin(
+                  std::bind(&WorldUuvPlugin::Update, this));
+            }
+
+    public: void Update()
+            {
+              printf("yo yo yo !\n");
             }
   };
   GZ_REGISTER_WORLD_PLUGIN(WorldUuvPlugin)
