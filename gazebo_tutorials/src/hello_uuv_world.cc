3e5bb4c96dcda128daa6e56fff576e58e9f29f02
Youssef <youssefkhaky@hotmail.com>
2020-07-08 07:13:13 -0400

    Cleanup

--- gazebo_tutorials/src/hello_uuv_world.cc
+++ gazebo_tutorials/src/hello_uuv_world.cc
@@ -27,45 +27,6 @@ namespace gazebo
   private: bool joined = false;
 
   private: gazebo::event::ConnectionPtr updateConnection;
-  // private: physics::JointPtr AddJoint(physics::WorldPtr _world,
-  //                              physics::ModelPtr _model,
-  //                              physics::LinkPtr _link1,
-  //                              physics::LinkPtr _link2,
-  //                              std::string _type,
-  //                              ignition::math::Vector3<double> _anchor,
-  //                              ignition::math::Vector3<double> _axis,
-  //                              double _upper, double _lower,
-  //                              bool _disableCollision)
-  //   {
-  //     physics::JointPtr joint;
-  //     joint = _world->Physics()->CreateJoint(_type, _model);
-  //     joint->Attach(_link1, _link2);
-  //     // load adds the joint to a vector of shared pointers kept
-  //     // in parent and child links, preventing joint from being destroyed.
-  //     joint->Load(_link1, _link2, ignition::math::Pose3<double>(_anchor, ignition::math::Quaternion<double>()));
-  //     joint->SetAxis(0, _axis);
-  //     joint->SetUpperLimit(0, _upper);
-  //     joint->SetLowerLimit(0, _lower);
-
-  //     if (_link1)
-  //       joint->SetName(_link1->GetName() + std::string("_") +
-  //                      _link2->GetName() + std::string("_joint"));
-  //     else
-  //       joint->SetName(std::string("world_") +
-  //                      _link2->GetName() + std::string("_joint"));
-  //     joint->Init();
-
-  //     // disable collision between the link pair
-  //     if (_disableCollision)
-  //     {
-  //       if (_link1)
-  //         _link1->SetCollideMode("fixed");
-  //       if (_link2)
-  //         _link2->SetCollideMode("fixed");
-  //     }
-
-  //     return joint;
-  //   }
 
 
   public: WorldUuvPlugin() : WorldPlugin(){}
@@ -74,30 +35,8 @@ namespace gazebo
       this->world = _world;
       this->socket = this->world->ModelByName("electrical_socket");
       this->plug = this->world->ModelByName("electrical_plug");
-      // DEBUG
-      // if (!(this->socket==NULL)){
-      //   printf("got socket  \n");
- 
-      // } else {
-      //   printf("failed socket  \n");
-
-      // }
       this->elecs = this->socket->GetLink("elecs");
       this->elecp = this->plug->GetLink("elecp");
-
-      // DEBUG
-      // physics::Link_V links = this->socket->GetLinks();
-      // for(physics::Link_V::iterator li = links.begin(); li != links.end(); ++li)
-      //   printf("%s  \n",(*li)->GetName().c_str());
-
-      //DEBUG
-      // if (!(this->elecp==NULL)){
-      //   printf("succed  \n");
- 
-      // } else {
-      //   printf("failed  \n");
-
-      // }
 
       this->updateConnection = gazebo::event::Events::ConnectWorldUpdateBegin(
           std::bind(&WorldUuvPlugin::Update, this));
@@ -147,9 +86,9 @@ namespace gazebo
       bool onSameVerticalLevel = abs(plugPosition[2] - socketPositon[2]) < 0.1;
       if (onSameVerticalLevel)
       {
-        return true;
         // printf("z=%.2f  \n", plugPosition[2]);
         // printf("Share same vertical level  \n");
+        return true;
       }
       return false;
     }
@@ -173,25 +112,6 @@ namespace gazebo
         prismaticJoint->SetUpperLimit(0, 2);
         prismaticJoint->SetLowerLimit(0, -0.1);
         prismaticJoint->Init();
-
-        // physics::ModelPtr model = _link->GetModel();
-        // physics::WorldPtr world = physics::get_world("default");
-        // physics::PhysicsEnginePtr engine = world->GetPhysicsEngine();
-        // this->joint = engine->CreateJoint("prismatic");
-        // this->joint->SetName(model->GetName() + "__perfect_lin_joint__");
-        // physics::LinkPtr worldLink = boost::dynamic_pointer_cast<physics::Link>(
-        //     world->GetByName("world"));
-        // math::Pose jointOrigin;
-        // this->joint->Load(worldLink, _link, jointOrigin);
-        // this->joint->Init();
-        // double magnitude = _vel.GetLength();
-        // this->joint->SetAxis(0, _vel.Normalize());
-        // this->joint->SetParam("fmax", 0, _maxForce);
-        // this->joint->SetParam("vel", 0, magnitude);
-
-
-        // prismaticJoint->SetParam("thread_pitch", 0, 1);
-        // prismaticJoint->SetVelocity();
       }
 
       if (joined){
