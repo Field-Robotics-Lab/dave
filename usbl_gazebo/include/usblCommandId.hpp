9da21b159fa00c1887514a809cdc46be73c89ea5
Carlos Agüero <caguero@openrobotics.org>
2020-09-25 22:31:19 +0200

    Add header guards.
    Signed-off-by: Carlos Agüero <caguero@openrobotics.org>

--- usbl_gazebo/include/usblCommandId.hpp
+++ usbl_gazebo/include/usblCommandId.hpp
@@ -1,5 +1,10 @@
+#ifndef DAVE_USBLCOMMANDLD_HPP_
+#define DAVE_USBLCOMMANDLD_HPP_
+
 typedef enum{
-    BATTERY_LEVEL = 1,
-    GO_TO = 2,
-    RESPONSE = 999
+  BATTERY_LEVEL = 1,
+  GO_TO         = 2,
+  RESPONSE      = 999
 } UsblPacketId;
+
+#endif
