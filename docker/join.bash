a485307d85950a0f471997d197f2441b26c17ce9
M1chaelM <mrmccarr@nps.edu>
2021-07-02 10:02:26 -0700

    resolve issue #113

--- docker/join.bash
+++ docker/join.bash
@@ -1,42 +1,9 @@
 #!/usr/bin/env bash
 #
-# Typical usage: ./join.bash dave_nvidia
+# Typical usage: ./join.bash <container_name> 
 #
 
-IMG=$(basename $1)
-
-# Get array of container id's based on ancestry
-CONTAINERS=$(docker ps -aqf "ancestor=${IMG}")
-#IFS=' '
-#read -ra CONTAINER_IDS <<< "$CONTAINERS"
-readarray -t CONTAINER_IDS <<<"$CONTAINERS"
-N=${#CONTAINER_IDS[@]}
-
-echo "$N Containers:"
-for C in "${CONTAINER_IDS[@]}"
-do
-    echo "    <${C}>"
-done
-
-if [ $N -lt 1 ]
-then
-    echo "Cannot find any containers with the ancestry <${IMG}>"
-    exit 1
-elif [ $N -eq 1 ]
-then
-    CONTAINER_ID=$CONTAINER_IDS
-    if [ ${#CONTAINER_ID} -lt 1 ]
-    then
-	echo "Cannot find any containers with the ancestry <${IMG}>"
-	exit 1
-    else
-	echo "Found just one container <${CONTAINER_ID}>.  Joining..."
-    fi
-else
-    CONTAINER_ID=${CONTAINER_IDS[0]}
-    echo "Found $N containers with ancestor <$IMG>.  Joining the first one <$CONTAINER_ID}> ..."
-fi
-
+CONTAINER_ID=$1
 
 xhost +
 docker exec --privileged -e DISPLAY=${DISPLAY} -e LINES=`tput lines` -it ${CONTAINER_ID} bash
