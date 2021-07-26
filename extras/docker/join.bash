7d4efafad3e64d484ea5fc8a6e2b69076f8d9c53
Brian Bingham <briansbingham@gmail.com>
2021-07-26 16:00:25 -0700

    Moving vim to end of dockerfile.  Adding missing dependency. Using rocker to run.  Adding defaults to run and build scripts.

--- extras/docker/join.bash
+++ extras/docker/join.bash
@@ -3,7 +3,13 @@
 # Typical usage: ./join.bash <container_name> 
 #
 
-CONTAINER_ID=$1
+# Default container
+CONTAINER_ID="dave_nvidia_runtime"
+if [ "$#" -ne 1 ]; then
+    echo "Joining default container <$CONTAINER_ID>"
+else
+    CONTAINER_ID=$1
+fi
 
 xhost +
 docker exec --privileged -e DISPLAY=${DISPLAY} -e LINES=`tput lines` -it ${CONTAINER_ID} bash
