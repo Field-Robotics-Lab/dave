65986c8869604f44a78ad468139d770140f00fd6
M1chaelM <mrmccarr@nps.edu>
2021-07-28 23:42:25 -0700

    undo ad hoc debugging to code_check

--- tools/code_check.sh
+++ tools/code_check.sh
@@ -154,6 +154,5 @@ if [ $xmlout -eq 1 ]; then
   (echo $CPPLINT_FILES | xargs python tools/cpplint.py 2>&1) \
     | python tools/cpplint_to_cppcheckxml.py 2> $xmldir/cpplint.xml
 elif [ $QUICK_CHECK -eq 0 ]; then
-  echo $CPPLINT_FILES
   echo $CPPLINT_FILES | xargs python3 tools/cpplint.py 2>&1
 fi
