53d23514ccc725d93b003de38121cd934865cf82
Woensug Choi <woensug.choi@gmail.com>
2021-07-30 10:34:45 +0900

    fix grammar for static code check

--- gazebo/dave_gazebo_model_plugins/include/dave_gazebo_model_plugins/usbl_transponder_plugin.h
+++ gazebo/dave_gazebo_model_plugins/include/dave_gazebo_model_plugins/usbl_transponder_plugin.h
@@ -1,6 +1,28 @@
+// Copyright (c) 2016 The dave Simulator Authors.
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef GAZEBO_TRANSPONDER_PLUGIN_HPP_
 #define GAZEBO_TRANSPONDER_PLUGIN_HPP_
 
+#include <ros/callback_queue.h>
+#include <ros/ros.h>
+#include <ros/subscribe_options.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Float64.h>
+
+#include <string>
 #include <thread>
 #include <random>
 
@@ -9,12 +31,6 @@
 
 #include <ignition/transport/Node.hh>
 #include <ignition/math/Pose3.hh>
-
-#include <ros/callback_queue.h>
-#include <ros/ros.h>
-#include <ros/subscribe_options.h>
-#include <std_msgs/String.h>
-#include <std_msgs/Float64.h>
 
 #include "dave_gazebo_model_plugins/UsblResponse.h"
 #include "dave_gazebo_model_plugins/UsblCommand.h"
@@ -32,7 +48,8 @@ namespace gazebo
       void temperatureRosCallback(const std_msgs::Float64ConstPtr &msg);
       void iisRosCallback(const std_msgs::StringConstPtr &msg);
       void cisRosCallback(const std_msgs::StringConstPtr &msg);
-      void commandRosCallback(const dave_gazebo_model_plugins::UsblCommandConstPtr& msg);
+      void commandRosCallback
+           (const dave_gazebo_model_plugins::UsblCommandConstPtr& msg);
       void queueThread();
 
     private:
