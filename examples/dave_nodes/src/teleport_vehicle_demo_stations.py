5b330b149e892a4a59dfa65870771b7b35addbf9
Dharini Dutia <dharini@openrobotics.org>
2022-03-03 14:29:58 -0800

    added control for vehicle orientation
    Signed-off-by: Dharini Dutia <dharini@openrobotics.org>

--- examples/dave_nodes/src/teleport_vehicle_demo_stations.py
+++ examples/dave_nodes/src/teleport_vehicle_demo_stations.py
@@ -9,6 +9,7 @@ import math
 
 # ROS/Gazebo imports
 import rospy
+import tf
 import sys
 from rosgraph_msgs.msg import Clock
 
@@ -23,9 +24,10 @@ class Teleport:
 
         # Parameters
         self.model_name = "rexrov"
-        self.demoLocationDict = {'home':[-21,-34,-108],
-                                 'electrical':[-30,-34,-108],
-                                 'mud':[-15,-34,-120]}
+        # Demo station vehicle location [x,y,z,roll,pitch,yaw]
+        self.demoLocationDict = {'home':[-21,-34,-108,0,0,2.5],
+                                 'electrical':[-30,-34,-108,0,0,3.141592],
+                                 'mud':[-15,-34,-120,0,0,0]}
 
         # Wait for ROS init
         self.t0 = rospy.get_time()
@@ -57,11 +59,17 @@ class Teleport:
         self.t0 = now
 
         try:
-            # Generate ModelState msg based on input
+            # Generate ModelState msg based on valid input
             pose = Pose()
             pose.position.x = self.demoLocationDict[station][0]
             pose.position.y = self.demoLocationDict[station][1]
             pose.position.z = self.demoLocationDict[station][2]
+            quaternion = tf.transformations.quaternion_from_euler(self.demoLocationDict[station][3],\
+                         self.demoLocationDict[station][4], self.demoLocationDict[station][5])
+            pose.orientation.x = quaternion[0]
+            pose.orientation.y = quaternion[1]
+            pose.orientation.z = quaternion[2]
+            pose.orientation.w = quaternion[3]
         except KeyError:
             rospy.logerr("Invalid demo station. Choose from: ["+",".join(self.demoLocationDict.keys())+"]")
             sys.exit()
@@ -69,8 +77,10 @@ class Teleport:
         # maneuver
         self.set_model_srv(ModelState(self.model_name, pose, Twist(), "world"))
 
-        rospy.loginfo("Vehicle is teleported to " + station + " location at (" \
-            + repr(pose.position.x) + "," + repr(pose.position.y) + "," + repr(pose.position.z) + ")")        
+        rospy.loginfo("Vehicle is teleported to " + station + " location at pose (" \
+            + repr(pose.position.x) + "," + repr(pose.position.y) + "," + repr(pose.position.z) \
+            + ") and" + " orientation ("+ repr(self.demoLocationDict[station][3]) + ","\
+            + repr(self.demoLocationDict[station][4]) + "," + repr(self.demoLocationDict[station][5]) + ")")
 
 
 if __name__ == '__main__':
